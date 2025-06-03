d056ff7b10fdce31f1b2bb7f1772b878b298a0e3
Dave Clouie Lising <164304088+daveclouie@users.noreply.github.com>
2025-06-03 17:55:40 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyATAj7J2tYbkwn9X2o6AbmZeHqHX-jE6OY",
+  authDomain: "lostfound-e304f.firebaseapp.com",
+  databaseURL: "https://lostfound-e304f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lostfound-e304f",
+  storageBucket: "lostfound-e304f.firebasestorage.app",
+  messagingSenderId: "963945204977",
+  appId: "1:963945204977:web:bd3b2cd467cb1cdf336d50",
+  measurementId: "G-GWNBNQQQDJ"
+};
